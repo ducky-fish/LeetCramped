7861526b24062e3a718d74684b631c306e96a357
ducky-fish <35616959+ducky-fish@users.noreply.github.com>
2022-05-08 21:42:30 -0600

    Create 412_Fizz_Buzz.py

---
+++ Python/412_Fizz_Buzz.py
@@ -0,0 +1,21 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        
+        modThree = 0
+        modFive = 0
+        for x in range(1,n+1):
+            modThree = x%3
+            modFive = x%5
+            
+            
+            if modThree + modFive == 0:
+                answer.append("FizzBuzz")
+            elif modThree == 0:
+                answer.append("Fizz")
+            elif modFive == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(x))
+        
+        return answer
